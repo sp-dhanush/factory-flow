369f893d9cd31e70dc38162474d43717dc479e46
Dhanush <dhanushsp616@gmail.com>
2026-08-18 19:50:58 +0530

    Updates

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  ignorePatterns: ['dist', 'node_modules', '*.min.js'],
+  env: {
+    browser: true,
+    es2020: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: '18.2'
+    }
+  },
+  rules: {
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    'react/prop-types': 'off'
+  }
+};
